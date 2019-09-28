0debcf0a056c998342290f91a417cb34adc9cfd4
sarah <s.j.condon01@gmail.com>
2019-09-28 18:55:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,17 @@
 class Dog
+  
   def name=(dogs_name)
     @name = dogs_name
   end
   
-    def name
-    @name
-  end 
-end 
-
-
-  def breed=(dogs_breed)
-    @breed = dogs_breed
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
   end
   
-    def breed
+  def breed
     @breed
   end 
-end 
+end
